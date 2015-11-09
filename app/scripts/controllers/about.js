b7f2ffa79689c9e8e92f86822bd73476fc05e266
Colin Boulanger <colin.boulanger@wiztivi.com>
2015-11-09 14:38:28 +0100

    second commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name soinsbienetreApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the soinsbienetreApp
+ */
+angular.module('soinsbienetreApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name soinsbienetreApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the soinsbienetreApp
+ */
+angular.module('soinsbienetreApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
